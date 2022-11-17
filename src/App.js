cbc6db16ccb7ebdee2b05f29ce6951e42af4ca83
Julia Diaków <tagitsu@gmail.com>
2022-11-17 10:46:16 +0100

    Add function that formats time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import Time from "./components/Time/Time";
+import { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState('');
+
+  const start = () => {
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue + 1)}, 1));
+  };
+  
+  const stop = () => {
+    clearInterval(timer);
+  };
+
+  const reset = () => {
+    setTime(0);
+  };
+
+  useEffect(() => {
+    return() => {
+      if(timer) clearInterval(timer)
+    }
+  }, []);
+
   return (
     <div className="app">
-      <Time />
+      <Time time={time} />
+      <button onClick={() => start()}>Start</button>
+      <button onClick={() => stop()}>Stop</button>
+      <button onClick={() => reset()}>Reset</button>
     </div>
   );
 };
--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,7 +1,28 @@
-const Time = () => {
+
+const Time = ({ time }) => {
+
+  let miliseconds, seconds, minutes, hours;
+  const getTime = (duration) => {
+    miliseconds = Math.floor((duration % 1000) / 10);
+    seconds = Math.floor((duration / 1000) % 60);
+    minutes = Math.floor((duration / 60000));
+    hours = Math.floor((duration / 360000));
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+  };
+  getTime(time);
 
   return(
-    <h1>it's the Time HH:mm:ss.ms</h1>
+    <div>
+        <p>{hours}:{minutes}:{seconds}.{miliseconds}</p>
+    </div>
   );
 };
 
